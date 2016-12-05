5cea372ddafc8b2686af2af2e8d13c9d6ebe3b3a
Joshua Pascascio <jpascasc@uci.edu>
2016-12-04 23:58:31 -0800

    Added Original, Instructor Provided Template
    This folder was the folder I worked off of in project 3 that demonstrated the sort of structure that was wanted for this implementation of a HashMap in C++ with a Hashfunction of style
    typedef std::function <unsigned int(const std::string&)> HashFunction
    
    This template for making a function was used from the builtin functional object in C++

---
+++ proj3/Original_Proj3_Template/gtest/HashMapTests.cpp
@@ -0,0 +1,20 @@
+// HashMapTests.cpp
+//
+// ICS 45C Fall 2016
+// Project #3: Maps and Legends
+//
+// Write unit tests for your HashMap class here.  I've provided one test already,
+// though I've commented it out, because it won't compile until you've implemented
+// three things: the HashMap constructor, the HashMap destructor, and HashMap's
+// size() member function.
+
+#include <gtest/gtest.h>
+#include "HashMap.hpp"
+
+
+//TEST(TestHashMap, sizeOfNewlyCreatedHashMapIsZero)
+//{
+//    HashMap empty;
+//    ASSERT_EQ(0, empty.size());
+//}
+
